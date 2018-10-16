37339abee3734a79a43826563bb64f5061d63bf0
KraysOK <bplucca@gmail.com>
2018-10-16 12:28:55 -0300

    nueva subida

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ti@1^w6x7pe2v!-^azku6fd)e4=nkon_ik8cl#86v(iv83)^5h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'kraysok.pythonanywhere.com']
 
 
 # Application definition
